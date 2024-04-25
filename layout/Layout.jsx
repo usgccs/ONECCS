472d99fec549c39aff112e5e22abbd7cc9daddb7
cpascasio <ceejay1110@gmail.com>
2024-04-25 22:51:00 +0800

    added: working routing madafaka

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -1,15 +1,18 @@
 // Layout.js
 import React from "react";
 import PageRouter from "../src/components/PageRouter";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const Layout = () => {
   return (
+    <Router>
     <div>
       {/* Your layout components */}
       {/* Navbar */}
       <PageRouter />
       {/* Footer */}
     </div>
+    </Router>
   );
 };
 
