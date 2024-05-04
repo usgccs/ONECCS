ac2b449961f8d460e2db7935f59b25f10effd558
Anna Katrina So <soannakatrina@gmail.com>
2024-05-04 23:08:11 +0800

    image carousel

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,11 +1,12 @@
 import useContentful from "../../../hooks/useContentful";
 import React, { useEffect, useState } from "react";
 import "./HomePage.css";
+import ImageCarousel from "../../components/ImageCarousel";
 
 const HomePage = () => {
     const [entry, setEntry] = useState(null);
     const fetchEntry = useContentful("1LtDDIwATLHp9ecde36Bej");
-  
+
     useEffect(() => {
       fetchEntry()
         .then((entry) => {
@@ -14,12 +15,12 @@ const HomePage = () => {
     })
         .catch(console.error);
     }, []);
-    
+
     return (
         <div className="header_main">
-        <h1>Home Page</h1>
+        {/* <h1>Home Page</h1> */}
 
-        <div>
+        {/* <div>
             {// Conditional rendering of data
                 entry && (
                     <div>
@@ -28,7 +29,11 @@ const HomePage = () => {
                     </div>
                 )
             }
-        </div>
+        </div> */}
+
+        <h2>
+            <ImageCarousel images = {['https://preview.redd.it/mr-fresh-the-cat-meme-v0-545c7nzqiyhc1.jpg?width=1080&crop=smart&auto=webp&s=5a7c2e0595c347e239740418ccdd1eada4960fde', 'https://yt3.googleusercontent.com/ZhGtIjcZJm0FNcO3TnAXwJJ_EnC9v_PIS_U4CyvRVG0WITvo-zq5UZXvSri-K2QyiEecuftCBpI=s900-c-k-c0x00ffffff-no-rj', 'https://kpopping.com/documents/27/4/4294/230716-NewJeans-Hanni-New-Jeans-at-Inkigayo-documents-4.jpeg?v=4b576', 'https://preview.redd.it/can-someone-photoshop-my-face-onto-this-meme-but-keep-the-v0-1kg7g9ub12fa1.jpg?width=905&format=pjpg&auto=webp&s=c9ea963d1ef07e9f925e2bcc5ed4e364ae031c12']} />
+        </h2>
 
         </div>
 
