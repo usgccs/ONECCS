1792b1b5462c952ab526ac2ac82983402e675e51
rainbow0017 <kyla.btst.05@gmail.com>
2024-05-06 23:16:18 +0800

    Update routes.jsx

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import HomePage from "../pages/homepage/HomePage";
 import ToDo from "../pages/todo/ToDo";
+import ContactPerson from "../pages/contactPerson/ContactPerson";
+
 
 const routes = [
-    {/* CTRL + CLICK the elements to go to the file */},
+    {/* CTRL + CLICK the elements to go to the file */ },
     { path: "/", element: <HomePage />, name: "Homepage" },
-    { path: "/todo", element: <ToDo/>, name: "Todo"},
+    { path: "/todo", element: <ToDo />, name: "Todo" },
+    { path: "/contactPerson", element: <ContactPerson />, name: "ContactPerson" }
+
 ];
 
 export default routes;
