f4cee641caf4aae8eb2f3c6232d9bf1e4339987c
cpascasio <ceejay1110@gmail.com>
2024-05-08 19:24:23 +0800

    restored app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Layout from "../layout/Layout";
+import "./App.css";
+import OrgDescription from "./components/orgDescription/OrgDescription";
+
+function App() {
+
+  return (
+    <>
+      <div className="w-screen h-screen">
+        <Layout />
+      </div>
+    </>
+  );
+}
+
+export default App;
