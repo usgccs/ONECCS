2593e02f696d06721bd5222dffbabf82ae62c9bf
Anna Katrina So <soannakatrina@gmail.com>
2024-05-13 19:14:29 +0800

    Added MSC org page

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -3,12 +3,14 @@ import HomePage from "../pages/homepage/HomePage";
 import ToDo from "../pages/todo/ToDo";
 import OrgDescription from "./orgDescription/OrgDescription";
 import CSG from "../pages/about-us/CSG/CSG"
+import MSC from "../pages/about-us/MSC/MSC"
 
 const routes = [
     {/* CTRL + CLICK the elements to go to the file */ },
     { path: "/", element: <HomePage />, name: "Homepage" },
     { path: "/todo", element: <ToDo />, name: "Todo" },
-    { path: "/about-us/csg", element: <CSG />, name: "Computer Studies Government" }
+    { path: "/about-us/csg", element: <CSG />, name: "Computer Studies Government" },
+    { path: "/about-us/msc", element: <MSC />, name: "Microsoft Student Community" }
     
 ];
 
