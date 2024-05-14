c18e8f90790e1b07d243735a891bed9c902c2378
Paula Pacheco <100471162+ennxxx@users.noreply.github.com>
2024-05-14 23:31:02 +0800

    feat: added data to PTS org page

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import HomePage from "../pages/homepage/HomePage";
 import ToDo from "../pages/todo/ToDo";
 import OrgDescription from "./orgDescription/OrgDescription";
-import CSG from "../pages/about-us/CSG/CSG"
+import CSG from "../pages/about-us/CSG/CSG";
+import PTS from "../pages/about-us/PTS/PTS";
 
 const routes = [
     {/* CTRL + CLICK the elements to go to the file */ },
     { path: "/", element: <HomePage />, name: "Homepage" },
     { path: "/todo", element: <ToDo />, name: "Todo" },
-    { path: "/about-us/csg", element: <CSG />, name: "Computer Studies Government" }
-    
+    { path: "/about-us/csg", element: <CSG />, name: "Computer Studies Government" },
+    { path: "/about-us/pts", element: <PTS />, name: "Peer Tutors Society" }
+
 ];
 
 export default routes;
