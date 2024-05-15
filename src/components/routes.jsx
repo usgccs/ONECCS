b51922e993dd59195cefdfdba9c44c7d2dc24f66
louiscadaaa <louis_cada@dlsu.edu.ph>
2024-05-15 21:24:42 +0800

    ACM css and jsx

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -3,13 +3,15 @@ import HomePage from "../pages/homepage/HomePage";
 import ToDo from "../pages/todo/ToDo";
 import OrgDescription from "./orgDescription/OrgDescription";
 import CSG from "../pages/about-us/CSG/CSG"
+import ACM from "../pages/about-us/ACM/ACM"
 
 const routes = [
     {/* CTRL + CLICK the elements to go to the file */ },
     { path: "/", element: <HomePage />, name: "Homepage" },
     { path: "/todo", element: <ToDo />, name: "Todo" },
-    { path: "/about-us/csg", element: <CSG />, name: "Computer Studies Government" }
-    
+    { path: "/about-us/csg", element: <CSG />, name: "Computer Studies Government" },
+    { path: "/about-us/acm", element: <ACM />, name: "DLSU ACM Student Chapter" }
+
 ];
 
 export default routes;
