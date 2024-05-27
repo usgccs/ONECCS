cb4d5587fd458522daed1ef713ef50be3360f7d3
louiscadaaa <louis_cada@dlsu.edu.ph>
2024-05-28 03:02:30 +0800

    Update some jsx and css files for cleanup

--- src/components/onlineSDFO/OnlineSDFO.jsx
+++ src/components/onlineSDFO/OnlineSDFO.jsx
@@ -4,7 +4,7 @@ import "./OnlineSDFO.css";
 const OnlineSDFO = () => {
 
     return (
-        <div className="loa__main_description">
+        <div className="sdfo__main_description">
             <div className="flex flex-col justify-center items-center w-full">
                 <button className="w-fit bg-[#24453E] text-white font-light m-4">
                     <a href="https://drive.google.com/drive/folders/15Xg-PGittLh4lv5haPk4rq0qMzBS6peJ" className="white-link">Submission of Non-Fraternity Contract and Drug Test Consent Form for Undergraduate Students</a>
