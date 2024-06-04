6f950feb87b531d0f686d201ede4e673d5ca0e42
Jose Simbillo <144484430+GithubSim13@users.noreply.github.com>
2024-06-04 20:33:28 +0800

    Update routes.jsx

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -3,12 +3,16 @@ import HomePage from "../pages/homepage/HomePage";
 import ToDo from "../pages/todo/ToDo";
 import OrgDescription from "./orgDescription/OrgDescription";
 import CSG from "../pages/about-us/CSG/CSG"
+import CATCH2T26 from "../pages/about-us/CATCH2T26/CATCH2T26"
+import LCCSG from "../pages/about-us/LCCSG/LCCSG"
 
 const routes = [
     {/* CTRL + CLICK the elements to go to the file */ },
     { path: "/", element: <HomePage />, name: "Homepage" },
     { path: "/todo", element: <ToDo />, name: "Todo" },
-    { path: "/about-us/csg", element: <CSG />, name: "Computer Studies Government" }
+    { path: "/about-us/csg", element: <CSG />, name: "Computer Studies Government" },
+    { path: "/about-us/catch2t26", element: <CATCH2T26 />, name: "CATCH2T26" },
+    { path: "/about-us/lccsg", element: <LCCSG />, name: "LCCSG" }
     
 ];
 
