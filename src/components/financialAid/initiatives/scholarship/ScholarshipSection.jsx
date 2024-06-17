595df6b56a00d6dd22fe5f5ee4f526471991263c
isaiahsam <isaiahpascual12@gmail.com>
2024-06-17 17:32:47 +0800

    added scholarship sections

---
+++ src/components/financialAid/initiatives/scholarship/ScholarshipSection.jsx
@@ -0,0 +1,61 @@
+const ScholarshipSection = () => {
+
+    return(
+        <div className="scholarshipmain">
+
+            <div className="s__description">
+                <h1>St. Mutien Marie Scholarship Grant </h1>
+
+                <div className="s_description">
+                    <p>The St. Mutien Marie Scholarship Grant for Graduates supports the University’s goal in making quality education accessible to those who are engaged in the profession of human formation and development.</p>
+                </div>
+
+                <h2>Benefits:</h2>
+                <p>50% discount on tuition (excluding miscellaneous and other fees)</p>
+                
+                <br />
+
+                <h2>Application Dates:</h2>
+                <p>February 10, 2023 (Friday) to March 10, 2023 (Friday)</p>
+                <p>Online Release of Results: 29 April 2023 (Saturday)</p>
+
+                <br />
+
+                <p>For the procedure and eligibility, click on the button below.</p>
+            </div>
+
+            <div className="s__description">
+                <h1>Vaugirard Scholarship Program </h1>
+
+                <div className="s_description">
+                    <p>This scholarship program aims to create a positive social transformation in the grantees. Beginning AY  2012-13,  the  University will provide fifty  (50)  new scholarship slots every year exclusively for students who graduated from Philippine public schools. The scholarship program provides an opportunity for these students to access Lasallian education through any baccalaureate degree program at De La Salle University.</p>
+                </div>
+
+                <h2>Benefits:</h2>
+                <p>Full waiver of tuition, miscellaneous and other fees from the first term until the conclusion of the undergraduate program.</p>
+                <br />
+                <p>Monthly stipend of Php 10,000 for the entire duration of the program.</p>
+                <br />
+                <p>An incentive equivalent to the stipend for one (1) month for those graduating with Latin honors.</p>
+                <br />
+                <p>Other privileges such as, but not exclusive to, advance enrollment, waiver of adjustment fee, waiver of graduation fees, and the likes.</p>
+                
+                <br />
+            </div>
+
+            <div className="s__description">
+                <h1>Br. Andrew Gonzalez Academic Scholarship </h1>
+
+                <div className="s_description">
+                    <p>High school students who ranked Top 1 (with highest honors) for each strand (STEM, ABM, HUMMS) of La Salle district schools and have passed the entrance exam will be awarded a full academic scholarship (tuition and fees) starting in their freshman year.</p>
+                </div>
+            </div>
+
+            
+
+        </div>
+    )
+
+}
+
+export default ScholarshipSection;
--- src/components/support/financialAid/financialAid.jsx
+++ src/components/support/financialAid/financialAid.jsx
@@ -2,6 +2,7 @@ import SectionHeader from "@/components/sectionHeader/SectionHeader";
 import InitiativeItem from "@/components/financialAid/initiatives/InitiativesItem";
 import InitiativesSection from "@/components/financialAid/initiatives/InitiativesSection";
 import TuitionFeePayment from "@/components/financialAid/tuitionFeePayment/TuitionFeePayment";
+import ScholarshipSection from "@/components/financialAid/initiatives/scholarship/ScholarshipSection";
 
 const FinancialAid = () => {
 
@@ -9,9 +10,8 @@ const FinancialAid = () => {
         <div className="flex flex-col justify-center items-center min-h-screen">
             <SectionHeader header={"Tuition Fee Payment Assistance"} />
             <TuitionFeePayment/>
-
-           
-
+            <SectionHeader header={"Scholarship"}/>
+            <ScholarshipSection/>
             <SectionHeader header={"USG Office of the Executive Treasurer Initiatives"} />
             <InitiativesSection/>
             
