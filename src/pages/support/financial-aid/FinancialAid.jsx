7bda325565c6416719efac0d4858105d2533b392
isaiahsam <isaiahpascual12@gmail.com>
2024-06-19 18:16:45 +0800

    Update FinancialAid.jsx

--- src/pages/support/financial-aid/FinancialAid.jsx
+++ src/pages/support/financial-aid/FinancialAid.jsx
@@ -1,11 +1,21 @@
-import SectionHeader from "../../../components/sectionHeader/SectionHeader";
+import SectionHeader from "@/components/sectionHeader/SectionHeader";
+import InitiativeItem from "@/components/financialAid/initiatives/InitiativesItem";
+import InitiativesSection from "@/components/financialAid/initiatives/InitiativesSection";
+import TuitionFeePayment from "@/components/financialAid/tuitionFeePayment/TuitionFeePayment";
+import ScholarshipSection from "@/components/financialAid/initiatives/scholarship/ScholarshipSection";
 import './FinancialAid'
 
 const FinancialAid = () => {
 
     return (
-        <div className="flex flex-col items-center min-h-screen">
-            <SectionHeader header={"Table of Contents"} />
+        <div className="flex flex-col justify-center items-center min-h-screen">
+            <SectionHeader header={"Tuition Fee Payment Assistance"} />
+            <TuitionFeePayment/>
+            <SectionHeader header={"Scholarship"}/>
+            <ScholarshipSection/>
+            <SectionHeader header={"USG Office of the Executive Treasurer Initiatives"} />
+            <InitiativesSection/>
+            
         </div>
     );
 }
