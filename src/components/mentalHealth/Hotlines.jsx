ec35fafebc38df57ffc9bc4de2c26e11e5d41b83
isaiahsam <isaiahpascual12@gmail.com>
2024-06-24 19:27:50 +0800

    mental health page

---
+++ src/components/mentalHealth/Hotlines.jsx
@@ -0,0 +1,25 @@
+const Hotlines = () => {
+    return (
+      <div className="w-full bg-[#24453E]">
+        <div className="w-full lg:w-[80%] h-full p-8 lg:p-32 flex flex-col xl:flex-row gap-10 lg:gap-24 mx-auto">
+          <div className="w-full xl:w-1/3 flex flex-col items-center text-white">
+            <p className="text-center text-xl font-semibold mb-4">Luzon-wide Landline Toll-Free</p>
+            <p className="text-center text-xl mb-4">1553</p>
+          </div>
+          <div className="w-full xl:w-1/3 flex flex-col items-center text-white">
+            <p className="text-center text-xl font-semibold mb-4">Globe/TM Subscribers</p>
+            <p className="text-center text-xl mb-4">0966-351-4518</p>
+            <p className="text-center text-xl mb-4">0917-899-8727</p>
+            <p className="text-center text-xl mb-4">0917-899-USAP</p>
+          </div>
+          <div className="w-full xl:w-1/3 flex flex-col items-center text-white">
+            <p className="text-center text-xl font-semibold mb-4">Smart/Sun/TNT Subscribers</p>
+            <p className="text-center text-xl mb-4">0908-639-2672</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Hotlines;
+  
--- src/pages/support/mental-health/MentalHealth.jsx
+++ src/pages/support/mental-health/MentalHealth.jsx
@@ -1,11 +1,15 @@
 import SectionHeader from "../../../components/sectionHeader/SectionHeader";
 import './MentalHealth'
+import Hotlines from "@/components/mentalHealth/Hotlines";
 
 const MentalHealth = () => {
 
     return (
         <div className="flex flex-col items-center min-h-screen">
             <SectionHeader header={"Table of Contents"} />
+
+            <SectionHeader header={"National Mental Health Crisis Hotline "} />
+            <Hotlines/>
         </div>
     );
 }
