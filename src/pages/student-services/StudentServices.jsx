3e4fc6f7510455fefbff92e935e31e139ea8890a
Andre <andre_aquino@dlsu.edu.ph>
2024-06-27 00:28:47 +0800

    removed previous changes to classes

--- src/pages/student-services/StudentServices.jsx
+++ src/pages/student-services/StudentServices.jsx
@@ -4,7 +4,7 @@ import SectionHeader from "../../components/sectionHeader/SectionHeader";
 const StudentServices = () => {
 
     return (
-        <div className="flex flex-col items-center min-h-screen sm:px-12 px-8 border border-red">
+        <div className="flex flex-col items-center min-h-screen">
             <SectionHeader header={"Enlistments"} />
             <EnlistmentSection />
             <SectionHeader header={"Enrollment Schedule"} />
