8297eaf6e4e2a37ddf96cd6e4e7124045cda2a3d
cpascasio <ceejay1110@gmail.com>
2024-07-25 04:42:55 +0800

    adjusted past projs

--- src/components/past-events/PastEventItem.jsx
+++ src/components/past-events/PastEventItem.jsx
@@ -4,7 +4,7 @@ const PastEventItem = ({ event }) => {
     return (
         <div className="flex flex-col items-center gap-4 justify-center md:w-96 sm:w-[340px] w-72 ">
             <img src={event.fields.projectImage.fields.file.url} alt="Event Logo" className="md:w-[22rem] sm:w-80 w-64 md:h-[22rem] sm:h-80 h-64 aspect-square object-cover" />
-            <span className='font-semibold text-2xl text-[#24453E] text-center border-2 p-2 rounded-full bg-white'>{event.fields.projectTitle}</span>
+            <span className='font-semibold text-2xl text-white text-center border-2 p-4 rounded-full'>{event.fields.projectTitle}</span>
         </div>
     );
 };
